d71e3b24bd2b4af0ceda4d3c761172c94564da8b
aminbenmansour <amin.benmansour.10@gmail.com>
2022-01-14 21:42:43 +0100

    creating a basic contract

---
+++ contracts/TodoList.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    /// creation of a state variable
+    /// public keywords adds a function to red the state variable 
+    uint public taskCount = 0;
+
+}
