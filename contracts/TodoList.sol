9b5c9909adf5716269226598bbb68048d707ef70
aminbenmansour <amin.benmansour.10@gmail.com>
2022-01-14 23:03:02 +0100

    createTask func & init in constructor

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,12 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     /// creation of a state variable
-    /// public keywords adds a function to red the state variable 
+    /// public keywords adds a function to read the state variable 
     uint public taskCount = 0;
 
     struct Task {
         uint id;
         string content;
         bool completed;
+    }
+
+    /// a mapping is key-value DS like hash table
+    /// public for adding a free get function
+    mapping(uint => Task) public tasks;
+
+
+    function createTask(string memory _content) public {
+        taskCount++;
+
+        tasks[taskCount] = Task(taskCount, _content, false);
     }
 }
