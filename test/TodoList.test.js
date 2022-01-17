f907cb6f1387274fe69418cd1989c7b6eb1fbdc3
aminbenmansour <amin.benmansour.10@gmail.com>
2022-01-17 21:00:07 +0100

    deployment test

---
+++ test/TodoList.test.js
@@ -0,0 +1,19 @@
+const { assert } = require("chai")
+
+const TodoList = artifacts.require('./TodoList.sol')
+
+
+contract('TodoList', (accounts) => {
+    before(async () => {
+        this.todoList = await TodoList.deployed()
+    })
+
+    it('deploys successfully', async () => {
+        const address = await this.todoList.address
+
+        assert.notEqual(address, 0x0)
+        assert.notEqual(address, '')
+        assert.notEqual(address, null)
+        assert.notEqual(address, undefined)
+    })
+})
