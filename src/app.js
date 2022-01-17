3fab54d2978345ca7de8179d0c7c87bb3cf7591e
aminbenmansour <amin.benmansour.10@gmail.com>
2022-01-17 19:26:26 +0100

    listing todos

--- src/app.js
+++ src/app.js
@@ -62,14 +62,13 @@ App = {
 
     render: async () => {
         // Prevent double render
-        console.log('abc')
         if (App.loading) {
             return
         }
 
         // Update app loading state
         App.setLoading(true)
-        console.log('def')
+
         // Render Account
         $('#account').html(App.account)
 
@@ -91,7 +90,40 @@ App = {
             loader.hide()
             content.show()
         }
-    }
+    },
+
+    renderTasks: async () => {
+        // Load the total task count from the blockchain
+        const taskCount = await App.todoList.taskCount()
+        const $taskTemplate = $('.taskTemplate')
+
+        // Render out each task with a new task template
+        for (var i = 1; i <= taskCount; i++) {
+            // Fetch the task data from the blockchain
+            const task = await App.todoList.tasks(i)
+            const taskId = task[0].toNumber()
+            const taskContent = task[1]
+            const taskCompleted = task[2]
+
+            // Create the html for the task
+            const $newTaskTemplate = $taskTemplate.clone()
+            $newTaskTemplate.find('.content').html(taskContent)
+            $newTaskTemplate.find('input')
+                .prop('name', taskId)
+                .prop('checked', taskCompleted)
+                .on('click', App.toggleCompleted)
+
+            // Put the task in the correct list
+            if (taskCompleted) {
+                $('#completedTaskList').append($newTaskTemplate)
+            } else {
+                $('#taskList').append($newTaskTemplate)
+            }
+
+            // Show the task
+            $newTaskTemplate.show()
+        }
+    },
 }
 
 $(() => {
